4a5ddd7cc82b85d4ab8e7d405cbca2c8a80428e9
Alex Zurek <alexander.j.zurek@gmail.com>
2023-12-01 08:43:46 -0500

    Update Elixir

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule AdventOfCode2023.MixProject do
     [
       app: :advent_of_code_2023,
       version: "0.1.0",
-      elixir: "~> 1.14",
+      elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
