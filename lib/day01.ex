5b2314c58a003992cd7d7c2ca64b10103e8ffe95
Alex Zurek <alexander.j.zurek@gmail.com>
2023-12-01 09:39:39 -0500

    Completed Day 1 - Part 1

--- lib/day01.ex
+++ lib/day01.ex
@@ -1,10 +1,22 @@
-defmodule Day01.Part1 do
-  def solve(_input) do
-  end
-end
+defmodule Day01 do
+  def part_1(input) do
+    input
+    |> String.split("\n", trim: true)
+    |> Enum.map(fn line ->
+      digits =
+        ~r/\d/
+        |> Regex.scan(line)
+        |> List.flatten()
 
-defmodule Day01.Part2 do
-  def solve(_input) do
+      first = hd(digits)
+      last = List.last(digits)
+      first <> last
+    end)
+    |> Enum.map(&String.to_integer/1)
+    |> Enum.sum()
+  end
+
+  def part_2(input) do
   end
 end
 
@@ -15,9 +27,9 @@ defmodule Mix.Tasks.Day01 do
     {:ok, input} = File.read("inputs/day01-input.txt")
 
     IO.puts("--- Part 1 ---")
-    IO.puts(Day01.Part1.solve(input))
+    IO.puts(Day01.part_1(input))
     IO.puts("")
     IO.puts("--- Part 2 ---")
-    IO.puts(Day01.Part2.solve(input))
+    IO.puts(Day01.part_2(input))
   end
 end
--- test/day01_test.exs
+++ test/day01_test.exs
@@ -1,16 +1,28 @@
 defmodule Day01Test do
   use ExUnit.Case
 
-  @example_input """
+  @example_input_1 """
+  1abc2
+  pqr3stu8vwx
+  a1b2c3d4e5f
+  treb7uchet
   """
 
-  @tag :skip
+  @example_input_2 """
+  two1nine
+  eightwothree
+  abcone2threexyz
+  xtwone3four
+  4nineeightseven2
+  zoneight234
+  7pqrstsixteen
+  """
+
   test "solves example input for part 1" do
-    assert Day01.Part1.solve(@example_input) == 42
+    assert Day01.part_1(@example_input_1) == 142
   end
 
-  @tag :skip
   test "solves example input for part 2" do
-    assert Day01.Part2.solve(@example_input) == 42
+    assert Day01.part_2(@example_input_2) == 281
   end
 end
