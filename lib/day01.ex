929eaf6f1ae9f275da824409abfe8829e64b580a
Alex Zurek <alexander.j.zurek@gmail.com>
2023-12-01 09:48:32 -0500

    Finished Day 1 - Part 2

--- lib/day01.ex
+++ lib/day01.ex
@@ -17,6 +17,38 @@ defmodule Day01 do
   end
 
   def part_2(input) do
+    input
+    |> String.split("\n", trim: true)
+    |> Enum.map(&parseDigits/1)
+    |> Enum.sum()
+  end
+
+  def parseDigits(line) do
+    ~r/(?=(\d|one|two|three|four|five|six|seven|eight|nine))/
+    |> Regex.scan(line)
+    |> List.flatten()
+    |> Enum.map(fn digit ->
+      case digit do
+        "one" -> "1"
+        "two" -> "2"
+        "three" -> "3"
+        "four" -> "4"
+        "five" -> "5"
+        "six" -> "6"
+        "seven" -> "7"
+        "eight" -> "8"
+        "nine" -> "9"
+        _ -> digit
+      end
+    end)
+    |> Enum.filter(fn digit ->
+      digit in ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+    end)
+    |> Kernel.then(fn nums ->
+      first = hd(nums)
+      last = List.last(nums)
+      String.to_integer(first <> last)
+    end)
   end
 end
 
