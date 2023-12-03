4d418c29778546576565fdbf0f4f43be0192844f
Alex Zurek <alexander.j.zurek@gmail.com>
2023-12-03 16:29:17 -0500

    Completed Day 3 - Part 1

--- test/day03_test.exs
+++ test/day03_test.exs
@@ -2,11 +2,125 @@ defmodule Day03Test do
   use ExUnit.Case
 
   @example_input """
+  467..114..
+  ...*......
+  ..35..633.
+  ......#...
+  617*......
+  .....+.58.
+  ..592.....
+  ......755.
+  ...$.*....
+  .664.598..
   """
 
-  @tag :skip
   test "solves example input for part 1" do
-    assert Day03.Part1.solve(@example_input) == 42
+    assert Day03.Part1.solve(@example_input) == 4361
+  end
+
+  test "alternate example 1" do
+    input = """
+    ........
+    .24..4..
+    ......*.
+    """
+
+    assert Day03.Part1.solve(input) == 4
+  end
+
+  test "alternate example 2" do
+    input = """
+    ........
+    .24$-4..
+    ......*.
+    """
+
+    assert Day03.Part1.solve(input) == 28
+  end
+
+  test "alternate example 3" do
+    input = """
+    11....11
+    ..$..$..
+    11....11
+    """
+
+    assert Day03.Part1.solve(input) == 44
+  end
+
+  test "alternate example 4" do
+    input = """
+    $......$
+    .1....1.
+    .1....1.
+    $......$
+    """
+
+    assert Day03.Part1.solve(input) == 4
+  end
+
+  test "alternate example 5" do
+    input = """
+    $......$
+    .11..11.
+    .11..11.
+    $......$
+    """
+
+    assert Day03.Part1.solve(input) == 44
+  end
+
+  test "alternate example 6" do
+    input = """
+    $11
+    ...
+    11$
+    ...
+    """
+
+    assert Day03.Part1.solve(input) == 22
+  end
+
+  test "alternate example 7" do
+    input = """
+    $..
+    .11
+    .11
+    $..
+    ..$
+    11.
+    11.
+    ..$
+    """
+
+    assert Day03.Part1.solve(input) == 44
+  end
+
+  test "alternate example 8" do
+    input = """
+    11.$.
+    """
+
+    assert Day03.Part1.solve(input) == 0
+  end
+
+  test "alternate example 9" do
+    input = """
+    .......5......
+    ..7*..*.....4*
+    ...*13*......9
+    .......15.....
+    ..............
+    ..............
+    ..............
+    ..............
+    ..............
+    ..............
+    21............
+    ...*9.........
+    """
+
+    assert Day03.Part1.solve(input) == 62
   end
 
   @tag :skip
