664081ba03436da1bccd7bb566bbcb53c0b8317e
Alex Zurek <alexander.j.zurek@gmail.com>
2023-12-04 15:05:40 -0500

    Completed Day 4 - Part 1

--- lib/day04.ex
+++ lib/day04.ex
@@ -1,6 +1,36 @@
 defmodule Day04.Part1 do
-  def solve(_input) do
+  def solve(input) do
+    input
+    |> String.split("\n", trim: true)
+    |> Enum.map(&String.split(&1, ":", trim: true))
+    |> Enum.map(fn [_, numbers] ->
+      [winners, values] =
+        numbers
+        |> String.split("|", trim: true)
+
+      winners =
+        winners
+        |> String.split(" ", trim: true)
+        |> Enum.map(&String.to_integer/1)
+
+      values =
+        values
+        |> String.split(" ", trim: true)
+        |> Enum.map(&String.to_integer/1)
+
+      numWinners =
+        Enum.reduce(values, 0, fn value, acc ->
+          if value in winners, do: acc + 1, else: acc
+        end)
+
+      calc_points(numWinners)
+    end)
+    |> Enum.sum()
   end
+
+  defp calc_points(0), do: 0
+  defp calc_points(1), do: 1
+  defp calc_points(count), do: :math.pow(2, count - 1)
 end
 
 defmodule Day04.Part2 do
