0e0b562e675db557a37b5bfe7ef16f3ab06bfcaa
Alex Zurek <alexander.j.zurek@gmail.com>
2023-12-07 13:57:05 -0500

    Completed Day 7 - Part 2

--- lib/day07.ex
+++ lib/day07.ex
@@ -64,7 +64,98 @@ defmodule Day07.Part1 do
 end
 
 defmodule Day07.Part2 do
-  def solve(_input) do
+  def solve(input) do
+    input
+    |> String.split("\n", trim: true)
+    |> Enum.map(fn line ->
+      [hand, bid] = String.split(line, " ", trim: true)
+
+      hand =
+        hand
+        |> String.split("", trim: true)
+        |> Enum.map(&determine_value/1)
+
+      %{hand: categorize_hand(hand), bid: String.to_integer(bid)}
+    end)
+    |> Enum.sort_by(fn %{hand: %{strength: strength, cards: [one, two, three, four, five]}} ->
+      {strength, one, two, three, four, five}
+    end)
+    |> Enum.with_index()
+    |> Enum.reduce(0, fn {%{bid: bid}, index}, acc ->
+      acc + (index + 1) * bid
+    end)
+  end
+
+  defp determine_value(card) do
+    case card do
+      "A" -> 14
+      "K" -> 13
+      "Q" -> 12
+      "J" -> 1
+      "T" -> 10
+      _ -> String.to_integer(card)
+    end
+  end
+
+  defp categorize_hand(hand) do
+    card_counts =
+      Enum.group_by(hand, fn c -> c end)
+
+    jokers = card_counts[1]
+
+    card_counts =
+      card_counts
+      |> Map.delete(1)
+      |> Map.to_list()
+
+    categorized_hand =
+      cond do
+        Enum.any?(card_counts, fn {_, cards} -> length(cards) == 5 end) ->
+          %{strength: 7, cards: hand}
+
+        Enum.any?(card_counts, fn {_, cards} -> length(cards) == 4 end) ->
+          %{strength: 6, cards: hand}
+
+        length(Enum.filter(card_counts, fn {_, cards} -> length(cards) == 2 end)) == 1 and
+            Enum.any?(card_counts, fn {_, cards} -> length(cards) == 3 end) ->
+          %{strength: 5, cards: hand}
+
+        Enum.any?(card_counts, fn {_, cards} -> length(cards) == 3 end) ->
+          %{strength: 4, cards: hand}
+
+        length(Enum.filter(card_counts, fn {_, cards} -> length(cards) == 2 end)) == 2 ->
+          %{strength: 3, cards: hand}
+
+        length(Enum.filter(card_counts, fn {_, cards} -> length(cards) == 2 end)) == 1 ->
+          %{strength: 2, cards: hand}
+
+        true ->
+          %{strength: 1, cards: hand}
+      end
+
+    handle_jokers(categorized_hand, jokers)
+  end
+
+  defp handle_jokers(hand, nil), do: hand
+
+  defp handle_jokers(%{strength: strength, cards: cards}, jokers) do
+    num_jokers = length(jokers)
+
+    cond do
+      num_jokers == 5 -> %{strength: 7, cards: cards}
+      strength == 6 and num_jokers == 1 -> %{strength: 7, cards: cards}
+      # strength == 5 and num_jokers == 2 -> %{strength: 7, cards: cards}
+      strength == 4 and num_jokers == 2 -> %{strength: 7, cards: cards}
+      strength == 4 and num_jokers == 1 -> %{strength: 6, cards: cards}
+      strength == 3 and num_jokers == 1 -> %{strength: 5, cards: cards}
+      strength == 2 and num_jokers == 3 -> %{strength: 7, cards: cards}
+      strength == 2 and num_jokers == 2 -> %{strength: 6, cards: cards}
+      strength == 2 and num_jokers == 1 -> %{strength: 4, cards: cards}
+      strength == 1 and num_jokers == 4 -> %{strength: 7, cards: cards}
+      strength == 1 and num_jokers == 3 -> %{strength: 6, cards: cards}
+      strength == 1 and num_jokers == 2 -> %{strength: 4, cards: cards}
+      strength == 1 and num_jokers == 1 -> %{strength: 2, cards: cards}
+    end
   end
 end
 
--- test/day07_test.exs
+++ test/day07_test.exs
@@ -1,16 +1,16 @@
 defmodule Day07Test do
   use ExUnit.Case
 
-  @example_input """
-  32T3K 765
-  T55J5 684
-  KK677 28
-  KTJJT 220
-  QQQJA 483
-  """
-
   test "solves example input for part 1" do
-    assert Day07.Part1.solve(@example_input) == 6440
+    input = """
+    32T3K 765
+    T55J5 684
+    KK677 28
+    KTJJT 220
+    QQQJA 483
+    """
+
+    assert Day07.Part1.solve(input) == 6440
   end
 
   test "solves example input for part 1 in reverse" do
@@ -43,8 +43,49 @@ defmodule Day07Test do
     assert Day07.Part1.solve(input) == 90
   end
 
-  @tag :skip
   test "solves example input for part 2" do
-    assert Day07.Part2.solve(@example_input) == 42
+    input = """
+    32T3K 765
+    T55J5 684
+    KK677 28
+    KTJJT 220
+    QQQJA 483
+    """
+
+    assert Day07.Part2.solve(input) == 5905
+  end
+
+  test "detecting a full house in part 2" do
+    input = """
+    2233J 42
+    """
+
+    assert Day07.Part2.solve(input) == 42
+  end
+
+  test "another example for part 2" do
+    input = """
+    2345A 1
+    Q2KJJ 13
+    Q2Q2Q 19
+    T3T3J 17
+    T3Q33 11
+    2345J 3
+    J345A 2
+    32T3K 5
+    T55J5 29
+    KK677 7
+    KTJJT 34
+    QQQJA 31
+    JJJJJ 37
+    JAAAA 43
+    AAAAJ 59
+    AAAAA 61
+    2AAAA 23
+    2JJJJ 53
+    JJJJ2 41
+    """
+
+    assert Day07.Part2.solve(input) == 6839
   end
 end
