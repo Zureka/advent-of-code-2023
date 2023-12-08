eeeef65dbbb8dc5f11835d2f2a9817c7857c47e1
Alex Zurek <alexander.j.zurek@gmail.com>
2023-12-08 10:48:51 -0500

    Complete Day 8 - Part 1

--- lib/day08.ex
+++ lib/day08.ex
@@ -1,5 +1,42 @@
 defmodule Day08.Part1 do
-  def solve(_input) do
+  def solve(input) do
+    [instructions, nodes] =
+      input
+      |> String.split("\n\n", trim: true)
+
+    instructions = String.split(instructions, "", trim: true)
+
+    instructions =
+      1..100
+      |> Enum.map(fn _ -> instructions end)
+      |> List.flatten()
+
+    nodes =
+      nodes
+      |> String.split("\n", trim: true)
+      |> Enum.with_index()
+      |> Enum.reduce(%{}, fn {line, index}, acc ->
+        [key, left, right] =
+          ~r/[A-Z]{3}/
+          |> Regex.scan(line)
+          |> List.flatten()
+
+        Map.put(acc, key, %{left: left, right: right, index: index})
+      end)
+
+    instructions
+    |> Enum.reduce_while({"AAA", 0}, fn step, {node, count} ->
+      if node == "ZZZ" do
+        {:halt, {node, count}}
+      else
+        %{left: left, right: right} = Map.get(nodes, node)
+
+        case step do
+          "L" -> {:cont, {left, count + 1}}
+          "R" -> {:cont, {right, count + 1}}
+        end
+      end
+    end)
   end
 end
 
@@ -15,9 +52,9 @@ defmodule Mix.Tasks.Day08 do
     {:ok, input} = File.read("inputs/day08-input.txt")
 
     IO.puts("--- Part 1 ---")
-    IO.puts(Day08.Part1.solve(input))
+    IO.inspect(Day08.Part1.solve(input))
     IO.puts("")
     IO.puts("--- Part 2 ---")
-    IO.puts(Day08.Part2.solve(input))
+    IO.inspect(Day08.Part2.solve(input))
   end
 end
