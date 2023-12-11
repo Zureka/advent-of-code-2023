8d3e5e7d6cace857ac65a0007eb523f36c76ccb1
Alex Zurek <alexander.j.zurek@gmail.com>
2023-12-10 20:46:00 -0500

    Complete Day 9 - Part 1

--- test/day09_test.exs
+++ test/day09_test.exs
@@ -2,11 +2,13 @@ defmodule Day09Test do
   use ExUnit.Case
 
   @example_input """
+  0 3 6 9 12 15
+  1 3 6 10 15 21
+  10 13 16 21 30 45
   """
 
-  @tag :skip
   test "solves example input for part 1" do
-    assert Day09.Part1.solve(@example_input) == 42
+    assert Day09.Part1.solve(@example_input) == 114
   end
 
   @tag :skip
