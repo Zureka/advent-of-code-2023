77a18030e647adfa6fb6381620d2831362f33018
Alex Zurek <alexander.j.zurek@gmail.com>
2023-12-10 20:53:58 -0500

    Completed Day 9 - Part 2

--- lib/day09.ex
+++ lib/day09.ex
@@ -28,7 +28,33 @@ defmodule Day09.Part1 do
 end
 
 defmodule Day09.Part2 do
-  def solve(_input) do
+  def solve(input) do
+    input
+    |> String.split("\n", trim: true)
+    |> Enum.map(fn line ->
+      String.split(line, " ", trim: true)
+      |> Enum.map(&String.to_integer/1)
+      |> process()
+    end)
+    |> Enum.sum()
+  end
+
+  defp process(values) do
+    cond do
+      Enum.all?(values, fn v -> v == 0 end) ->
+        0
+
+      true ->
+        [first | _] = values
+
+        end_value =
+          values
+          |> Enum.chunk_every(2, 1, :discard)
+          |> Enum.map(fn [a, b] -> a - b end)
+          |> process()
+
+        first + end_value
+    end
   end
 end
 
