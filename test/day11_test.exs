78196f2917abbbea9a742b7b2c4fcb581f9d47da
Alex Zurek <alexander.j.zurek@gmail.com>
2023-12-22 13:54:31 -0500

    Complete Day 11 - both parts

--- test/day11_test.exs
+++ test/day11_test.exs
@@ -2,11 +2,30 @@ defmodule Day11Test do
   use ExUnit.Case
 
   @example_input """
+  ...#......
+  .......#..
+  #.........
+  ..........
+  ......#...
+  .#........
+  .........#
+  ..........
+  .......#..
+  #...#.....
   """
 
-  @tag :skip
   test "solves example input for part 1" do
-    assert Day11.Part1.solve(@example_input) == 42
+    assert Day11.Part1.solve(@example_input) == 374
+  end
+
+  test "part 1 - calculates properly when galaxies are next to each other" do
+    input = "###"
+    assert Day11.Part1.solve(input) == 4
+  end
+
+  test "part 1 - calculates properly when galaxies are next to each other v2" do
+    input = "####"
+    assert Day11.Part1.solve(input) == 10
   end
 
   @tag :skip
